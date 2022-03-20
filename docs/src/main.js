83768931894ad08eb3bfe820d9f44de18e0c57a1
DatNgo <ngominhdat110115@gmail.com>
2022-03-20 12:57:15 +0700

    complete game

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,15 +1,10 @@
 const canvas = document.querySelector('#canvas1')
-const c = canvas.getContext('2d') 
+const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-class Player{
+class Player {
     constructor() {
-        this.position = {
-            x: 200,
-            y: 200
-        }
-
         this.velocity = {
             x: 0,
             y: 0
@@ -17,22 +12,137 @@ class Player{
 
         const image = new Image()
         image.src = './asset/starship.png'
-
-        this.image = image
-        this.width = 100
-        this.height = 100
+        image.onload = () => {
+            const scale = 1
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.height - 20
+            }
+        }
     }
 
     draw() {
-       c.drawImage(this.image, this.position.x, this.position.y) 
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+        }
+    }
+}
+
+class Projectile {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 3
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'Red'
+        c.fill()
+        c.closePath()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        // if (this.position.x < 0 || this.position.x > canvas.width) {
+        //     projectiles.
+        // }
     }
 }
 
 const player = new Player()
+const projectiles = []
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate)
-    player.draw()
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    projectiles.forEach((projectile, index) => {
+        if (projectile.position.y <= 0) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0);
+        } else {
+            projectile.update()
+        }
+    })
+
+    if (keys.a.pressed == true && player.position.x > 0) {
+        player.velocity.x = -5
+    } else if (keys.d.pressed == true && player.position.x + player.width < canvas.width) {
+        player.velocity.x = 5
+    } else {
+        player.velocity.x = 0
+    }
 }
 
 animate()
+
+addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'a':
+            keys.a.pressed = true
+            break;
+        case 'd':
+            keys.d.pressed = true
+            break;
+        case ' ':
+            keys.space.pressed = true
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x + player.width / 2,
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -5
+                }
+            }))
+
+            break;
+
+        default:
+            break;
+    }
+})
+
+addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'a':
+            keys.a.pressed = false
+            break;
+        case 'd':
+            keys.d.pressed = false
+            break;
+        case ' ':
+            keys.space.pressed = false
+            break;
+
+        default:
+            break;
+    }
+})
